232979a19f045a0216fd23c82c5169dbf1d23a43
krisrandall <kris.randall@gmail.com>
2017-02-20 21:43:26 +1000

    code in other shortcode replacements but not yet tested, need to implement the endpoint routing first   (and then also the cart interface functions) .. but getting there ..

--- parcel4me/parcel4me-shortcodes-class.php
+++ parcel4me/parcel4me-shortcodes-class.php
@@ -16,11 +16,14 @@ class Parcel4me_Shortcodes {
       return plugins_url() . '/parcel4me/lib/';
     }
 
+
     function add_all_scripts() {
       // scripts required for all components
       wp_enqueue_script( 'webcomponentsjs', base_uri() . 'webcomponentsjs/webcomponents.min.js' );
     }
     add_action( 'wp_enqueue_scripts', 'add_all_scripts' );
+
+
 
     // [p4m-login]
     function p4m_login_func( $atts ){
@@ -32,6 +35,35 @@ class Parcel4me_Shortcodes {
             </p4m-login>';
     }
     add_shortcode( 'p4m-login', 'p4m_login_func' );
+
+
+    // [p4m-signup]
+    function p4m_signup_func( $atts ) {
+      echo '<link rel="import" href="' . base_uri() . 'p4m-widgets/p4m-register/p4m-register.html" />';
+      echo '<p4m-register></p4m-register>';
+    }
+    add_shortcode( 'p4m-signup', 'p4m_signup_func' );
+
+
+    // [p4m-checkout]
+    function p4m_checkout_func( $atts ) {
+      $gfs_access_token = (array_key_exists('gfsCheckoutToken', $_COOKIE) ? $_COOKIE['gfsCheckoutToken'] : '');
+      echo '<link rel="import" href="' . base_uri() . 'p4m-widgets/p4m-checkout/p4m-checkout.html" />';
+      echo '<p4m-checkout use-paypal="true" 
+                          use-gfs-checkout="true"
+                          session-id="' . session_id() . '"
+                          gfs-access-token="' . $gfs_access_token . '" >
+            </p4m-checkout>';
+    }
+    add_shortcode( 'p4m-checkout', 'p4m_checkout_func' );
+
+
+    // [p4m-payment-complete]
+    function p4m_pc_func( $atts ) {
+      echo '<link rel="import" href="' . base_uri() . 'p4m-widgets/p4m-get-html/p4m-get-html.html" />';
+      echo '<p4m-get-html content-url="' . P4M\Settings::getPublic( 'Server:P4M_OID_SERVER' ) .'/Thankyou/Thankyou.html">';
+    }
+    add_shortcode( 'p4m-payment-complete', 'p4m_pc_func' );
 
 
   }
