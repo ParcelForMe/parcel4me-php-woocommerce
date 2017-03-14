18c7efea758c87111a35cea3a76b1ee37182e8fe
krisrandall <kris.randall@gmail.com>
2017-03-14 15:14:02 +1000

    install-widgets.sh becomes reinstall-widgets.sh (and also include the latest widget changes)

--- parcel4me-woo/reinstall-widgets.sh
+++ parcel4me-woo/reinstall-widgets.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 echo
+echo "About to remove the current version of the P4M widgets ..."
+echo "Press any key to proceed"
+read -n1 -r
+
+rm -rf ./lib
+
+echo
 echo "Installing the P4M widgets from https://github.com/ParcelForMe/p4m-widgets/ into ./lib/"
 echo "Press any key to proceed"
 read -n1 -r 
@@ -13,4 +20,5 @@ cd p4m-widgets
 bower install 
 mv bower_components/* ..
 rmdir bower_components
+rm -rf .git*
 
